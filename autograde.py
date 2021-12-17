1db6d79a33ed13d39d915c36c97b1210ab4e0ec4
xxl <549506247@qq.com>
2021-12-17 21:02:55 +0800

    修改了对读文件的末尾没有换行符和文件末尾多余空行造成的问题

--- autograde.py
+++ autograde.py
@@ -5,6 +5,10 @@ def get_data_from_file(file_name):
     try:
         with open(file_name, encoding='utf-8') as f:
             lines = f.readlines()
+            while lines[-1] == '\n':
+                lines = lines[:-1]
+            if lines[-1][-1] != '\n':
+                lines[-1][-1] += '\n'
             return lines[0], [line[:-1] for line in lines[1:]]
     except IOError as e:
         print('读取文件异常', e)
@@ -38,26 +42,30 @@ def get_state_set(lines):
 def grade(start_set, final_set, transfers, inputs):
     if len(start_set) != 1:
         return '开始状态不唯一'
-    res = list()
+    res, f = list(), list()
     for sentence in inputs:
         curr = start_set[0]
         for word in sentence:
             if curr == 'N':
-                res.append('错误，自动机卡住，当前状态为'+curr+'输入字符'+word+'\n所在句子为：'+sentence)
+                res.append('错误，自动机卡住，当前状态为' + curr + '输入字符' + word + '\n所在句子为：' + sentence)
+                f.append(False)
                 break
             if word == '0':
                 curr = transfers[curr][0]
             elif word == '1':
                 curr = transfers[curr][1]
             else:
-                res.append('错误，读入的字符串中包含非0非1的字符：'+word)
+                res.append('错误，读入的字符串中包含非0非1的字符：' + word)
+                f.append(False)
                 break
 
         if curr not in final_set:
-            res.append('错误，字符串读完，未停止在终止状态，当前状态为:'+curr+'\n所在句子为：'+sentence)
+            res.append('错误，字符串读完，未停止在终止状态，当前状态为:' + curr + '\n所在句子为：' + sentence)
+            f.append(False)
         else:
             res.append('句子{}成功接收！'.format(sentence))
-    return res
+            f.append(True)
+    return res, f
 
 
 def mini(num, states_len):
@@ -68,13 +76,25 @@ def add_parameters():
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--ans_file', type=str, default='ans.txt')
     parser.add_argument('-s', '--sentences_file', type=str, default='sentences.txt')
-    parser.add_argument('-d', "--debug", type=str, default=False)
+    parser.add_argument('-d', "--debug", type=str, default=True)
     return parser.parse_args()
 
 
 def print_results(res):
     for item in ans:
         print(item)
+
+
+def grade_scores(f):
+    standard = [True] * 10 + [False] * 10
+    score = 0
+    fail_info = list()
+    for i in range(20):
+        if f[i] == standard[i]:
+            score += 1
+        else:
+            fail_info.append(i)
+    return score, fail_info
 
 
 if __name__ == '__main__':
@@ -86,12 +106,14 @@ if __name__ == '__main__':
     states_num = int(states_num[:-1])
 
     S, F, table = get_state_set(contents)
-    ans = grade(S, F, table, sentences)
-
+    ans, f_ans = grade(S, F, table, sentences)
     if args.debug:
         print_results(ans)
 
-    if mini(states_num, len(contents)):
-        print('√该自动机是极小的')
-    else:
-        print('×自动机的状态不是极小的')
+    #  评分系统
+    # scores, error_info = grade_scores(f_ans)
+    print('End!')
+    # if mini(states_num, len(contents)):
+    #     print('√该自动机是极小的')
+    # else:
+    #     print('×自动机的状态不是极小的')
